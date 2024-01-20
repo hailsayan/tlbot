2dc174184a02a77fd9d92ed433394bded5e330bc
Psyon <143930298+hailsayan@users.noreply.github.com>
2024-01-20 15:26:42 +0330

    test1

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const TelegramBot = require('node-telegram-bot-api');
 const axios = require ('axios');
 
-
 const token = '6934019405:AAHtSY8hZPGEGV79PcKOoYQ9u0Ant_cEa8Q';
+
+const bot = new TelegramBot(token, {polling: true});
+
+bot.on('message', async (msg) => {
+    console.log('hello');
+})
