676c924c92d528bbbe5d5602063a03997a6a59b5
Psyon <143930298+hailsayan@users.noreply.github.com>
2024-01-20 16:09:24 +0330

    v1

--- index.js
+++ index.js
@@ -6,9 +6,23 @@ const token = '6934019405:AAHtSY8hZPGEGV79PcKOoYQ9u0Ant_cEa8Q';
 const bot = new TelegramBot(token, {polling: true});
 
 bot.on('message', async (msg) => {
-    // console.log(msg);
     const chatId = msg.chat.id;
-    const usetInput = msg.text;
-    const mess = 'سلام رییس';
-    bot.sendMessage(chatId, mess);
-})
+    const userInput = msg.text;
+    try {
+        const response = await axios.get(
+          `https://api.openweathermap.org/data/2.5/weather?q=${userInput}&appid=926d3e483eab1a0373d6fa0ea4a2c794`
+        );
+        const data = response.data;
+        const weather = data.weather[0].description;
+        const temperature = data.main.temp - 273.15;
+        const city = data.name;
+        const humidity = data.main.humidity;
+        const pressure = data.main.pressure;
+        const windSpeed = data.wind.speed;
+        const message = `The weather in ${city} is ${weather} with a temperature of ${temperature.toFixed(2)}°C. The humidity is ${humidity}%, the pressure is ${pressure}hPa, and the wind speed is ${windSpeed}m/s.`;
+    
+        bot.sendMessage(chatId, message);
+    }   catch (error) {
+        bot.sendMessage(chatId, 'sorry, city doesnt exist...');
+    }
+});
