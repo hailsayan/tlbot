44fc16f4716705ac9b4cd2ad2eb95740f0e9af85
Psyon <143930298+hailsayan@users.noreply.github.com>
2024-01-20 15:40:51 +0330

    first message

--- index.js
+++ index.js
@@ -6,5 +6,9 @@ const token = '6934019405:AAHtSY8hZPGEGV79PcKOoYQ9u0Ant_cEa8Q';
 const bot = new TelegramBot(token, {polling: true});
 
 bot.on('message', async (msg) => {
-    console.log('hello');
+    // console.log(msg);
+    const chatId = msg.chat.id;
+    const usetInput = msg.text;
+    const mess = 'سلام رییس';
+    bot.sendMessage(chatId, mess);
 })
